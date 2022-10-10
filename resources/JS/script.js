7448307eaafcf69645475b3de0f8c016116321a8
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-10-10 20:23:08 +0100

    Clear linter errors

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,45 +1,44 @@
-//Declare Variables
-let menuButton = document.getElementById("menu-button");
-let menu = document.getElementById("menu-id");
-let closeButton = document.getElementById("close");
+// Declare Variables
 
-//Create event listener functions
-function showMenu () {
-    menu.classList.replace("hide", "show");
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu-id');
+const closeButton = document.getElementById('close');
+
+// Create event listener functions
+function showMenu() {
+  menu.classList.replace('hide', 'show');
 }
 
-function hideMenu () {
-    menu.classList.replace("show", "hide");
+function hideMenu() {
+  menu.classList.replace('show', 'hide');
 }
 
-//Add event listeners to html elements
-menuButton.addEventListener("click", showMenu);
-closeButton.addEventListener("click", hideMenu)
+// Add event listeners to html elements
+menuButton.addEventListener('click', showMenu);
+closeButton.addEventListener('click', hideMenu);
 
-let menuItems = document.getElementsByClassName("menu-item");
-for (let i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener("click", hideMenu)
+const menuItems = document.getElementsByClassName('menu-item');
+for (let i = 0; i < menuItems.length; i += 1) {
+  menuItems[i].addEventListener('click', hideMenu);
 }
 
-//Highligh visible section of webpage on window scroll
-const sections = document.querySelectorAll("section");
-console.log(sections);
-const navLi = document.getElementsByClassName("nav-item");
-console.log(navLi);
+// Highligh visible section of webpage on window scroll
+const sections = document.querySelectorAll('section');
+const navLi = document.getElementsByClassName('nav-item');
 window.onscroll = () => {
-  let current = "";
+  let current = '';
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id"); 
+    if (window.scrollY >= sectionTop - 60) {
+      current = section.getAttribute('id');
     }
   });
 
-  for (let i = 0; i < navLi.length; i++) {
-    navLi[i].classList.remove("active");
+  for (let i = 0; i < navLi.length; i += 1) {
+    navLi[i].classList.remove('active');
     if (navLi[i].classList.contains(current)) {
-      navLi[i].classList.add("active");
+      navLi[i].classList.add('active');
     }
   }
 };
