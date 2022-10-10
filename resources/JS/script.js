186979e6f51cbb262777e6ce7c2ed6dd0f51e873
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-10-10 19:58:24 +0100

    Add javascrip to highlight section on user scroll

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -20,3 +20,26 @@ let menuItems = document.getElementsByClassName("menu-item");
 for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener("click", hideMenu)
 }
+
+//Highligh visible section of webpage on window scroll
+const sections = document.querySelectorAll("section");
+console.log(sections);
+const navLi = document.getElementsByClassName("nav-item");
+console.log(navLi);
+window.onscroll = () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); 
+    }
+  });
+
+  for (let i = 0; i < navLi.length; i++) {
+    navLi[i].classList.remove("active");
+    if (navLi[i].classList.contains(current)) {
+      navLi[i].classList.add("active");
+    }
+  }
+};
